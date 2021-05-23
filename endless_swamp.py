c0ac870869aae539513f228cfb7fe9b1f99525e2
AChar95 <allancharles95@outlook.com>
2021-05-23 22:29:54 +0100

    retrieving username

--- endless_swamp.py
+++ endless_swamp.py
@@ -1,5 +1,10 @@
+from random import randrange
+
 def main():
-    print()
+    userName = input("Please enter your character's name: ")
+    print(''' 
+          Welcome to the endless swamp, a place of infantismal sludge as far as the eye can see.
+          ''')
     
 if __name__ == "__main__":
     main()
