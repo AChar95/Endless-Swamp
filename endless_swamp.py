57d74b7ca877a0173a19a88473335408e67ccb59
AChar95 <allancharles95@outlook.com>
2021-05-26 21:51:19 +0100

    added demo for importing json

--- endless_swamp.py
+++ endless_swamp.py
@@ -1,10 +1,12 @@
 from random import randrange
+from plot import plot
 
 def main():
     userName = input("Please enter your character's name: ")
     print(''' 
           Welcome to the endless swamp, a place of infantismal sludge as far as the eye can see.
-          ''')
+          The jolly wagon driver waved goodbye before whipping the mule. 'Good luck on your hunt %s' he shouted
+          ''' %(userName))
     
 if __name__ == "__main__":
     main()
