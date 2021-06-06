1e73450bc8c4fdc27551b11eb685300c3e3250d7
AChar95 <allancharles95@outlook.com>
2021-06-06 16:19:48 +0100

    Added support for win logic

--- win_mechanic.py
+++ win_mechanic.py
@@ -1,6 +1,7 @@
-def winMechanic ():
+def winMechanic (distance):
     #Setting up win mechanic function
-    print()
+    if int(distance) == 0:
+        print("Congratulations, you have found the chest")
     
 if __name__ == "__main__":
     print("This file should only be imported and not called directly, please run endless_swamp.py")
