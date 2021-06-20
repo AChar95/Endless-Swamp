49722a21b3ab1a5429d0b71602d4f07eec52ff61
AChar95 <allancharles95@outlook.com>
2021-06-20 11:02:46 +0100

    adding testing for chest class

--- testing.py
+++ testing.py
@@ -1,5 +1,6 @@
 import unittest
 from user import User
+from chest import Chest
 
 class TestingUserMethod(unittest.TestCase):
     def testingXCoordinate(self):
@@ -13,6 +14,15 @@ class TestingUserMethod(unittest.TestCase):
     def testingUserName(self):
         user = User('Keiran', 3, 5)
         self.assertEqual(user.username, 'Keiran')
+    
+    def testingChestXCoordinate(self):
+        chest = Chest(3,5)
+        self.assertEqual(chest.chestX, 3)
+    
+    def testChestYCoordinate(self):
+        chest = Chest(3,5)
+        self.assertEqual(chest.chestY, 5)
+        
         
 if __name__ == '__main__':
     unittest.main()
